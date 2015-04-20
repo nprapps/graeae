9245d3503c684a994d3a526b76bffa576676e38e
Christopher Groskopf <staringmonkey@gmail.com>
2015-04-20 10:40:44 -0400

    Remove database test harness for now.

--- tests/test_homepage_scraper.py
+++ tests/test_homepage_scraper.py
@@ -9,34 +9,6 @@ from nose.tools import assert_equal
 from scrapers.homepage import HomepageScraper
 from time import time
 
-
-class TestScraperDB(unittest.TestCase):
-    """
-    Test the scraper
-    """
-    @classmethod
-    def setup_class(klass):
-        """This method is run once for each class before any tests are run"""
-        klass.temp_db_name = 'graeae_%s' % int(time())
-        local('createdb %s' % klass.temp_db_name)
-
-    @classmethod
-    def teardown_class(klass):
-        """This method is run once for each class _after_ all tests are run"""
-        local('dropdb %s' % klass.temp_db_name)
-
-    def setUp(self):
-        self.db = dataset.connect('postgres:///%s' % self.temp_db_name)
-        self.scraper = HomepageScraper()
-
-    # def test_db_write(self):
-    #     articles = self.scraper.scrape_homepage()
-    #     api_entries = self.scraper.scrape_api_entries(articles)
-    #     self.scraper.write(self.db, articles, api_entries)
-    #     rows = list(self.db['homepage'].all())
-    #     self.assertGreater(len(rows), 0)
-
-
 class TestScrapeHomepage(unittest.TestCase):
     def setUp(self):
         self.scraper = HomepageScraper()
