21e29a22ad0cfc3fde5e4f3f72b0b29e2ede0f6b
David Eads <davideads@gmail.com>
2015-04-21 17:21:11 -0400

    handle pagination in seamus api, closes #74

--- scrapers/seamus/scraper.py
+++ scrapers/seamus/scraper.py
@@ -10,6 +10,7 @@ from app_config import get_secrets
 from models import Story
 
 SECRETS = get_secrets()
+SEAMUS_API_PAGE_SIZE = 20
 
 class SeamusScraper:
     def __init__(self):
@@ -23,16 +24,38 @@ class SeamusScraper:
         print '---------------'
 
         if not kwargs:
+            stories = self._get_stories_from_api()
+        else:
+            element = PyQuery(parser='xml', **kwargs)
+            story_elements = element.find('story')
+            stories = self._extract_stories(story_elements)
+
+        return stories
+
+    def _get_stories_from_api(self):
+        startNum = 1
+        stories = []
+        while True:
             response = requests.get('http://api.npr.org/query', params={
                 'date': 'current',
                 'orgId': '1',
-                'apiKey': SECRETS['NPR_API_KEY']})
+                'apiKey': SECRETS['NPR_API_KEY'],
+                'numResults': 20,
+                'startNum': startNum,
+            })
 
             element = PyQuery(response.content, parser='xml')
-        else:
-            element = PyQuery(parser='xml', **kwargs)
+            story_elements = element.find('story')
 
-        story_elements = element.find('story')
+            if len(story_elements):
+                stories += self._extract_stories(story_elements)
+                startNum += SEAMUS_API_PAGE_SIZE
+            else:
+                break
+
+        return stories
+
+    def _extract_stories(self, story_elements):
         stories = []
 
         for story_el in story_elements:
@@ -41,6 +64,7 @@ class SeamusScraper:
             stories.append(Story(story_el, self.run_time))
 
         return stories
+
 
     def write(self, db, stories):
         """
