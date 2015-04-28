31595cce5c8e9aff12a290ab311ee880eb430e8e
David Eads <davideads@gmail.com>
2015-04-28 13:25:47 -0400

    add new tests to seamus scraper, closes #95

--- tests/test_seamus_scraper.py
+++ tests/test_seamus_scraper.py
@@ -29,3 +29,16 @@ class TestScrapeSeamus(unittest.TestCase):
 
     def test_canonical_url(self):
         self.assertEqual(self.stories[0].canonical_url, 'http://www.npr.org/blogs/thetwo-way/2015/04/21/401157787/top-stories-ex-egypt-leader-sentenced-blue-bell-recall-expands')
+
+    def test_has_lead_art(self):
+        self.assertEqual(self.stories[0].has_lead_art, False)
+        self.assertEqual(self.stories[6].has_lead_art, True)
+
+    def test_lead_art_provider(self):
+        print self.stories[6].canonical_url
+        self.assertEqual(self.stories[6].lead_art_provider, 'Mohamed El-Shahed/AFP/Getty Images')
+
+    def test_lead_art_url(self):
+        print self.stories[6].lead_art_url
+        self.assertEqual(self.stories[6].lead_art_url, 'http://media.npr.org/assets/img/2015/04/21/morsi-court_wide-27506a416e57aafb2da92954aa1b28a88df7d467.jpg')
+
