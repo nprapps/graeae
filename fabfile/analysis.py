674dc8ee740ccef340234ccb389c2868cc306915
Livia <llabate@npr.org>
2015-05-27 13:55:11 -0400

    Combined Seamus and Facebook insights

--- fabfile/analysis.py
+++ fabfile/analysis.py
@@ -17,3 +17,23 @@ def get_insights():
 			""")
 	dataset.freeze(result, format='csv', filename='output/insights.csv')
 
+@task
+def get_insights_and_art():
+	"""
+	gets insights and art and writes csv
+	"""
+	db = dataset.connect(app_config.POSTGRES_URL)
+	result = db.query("""
+	select f1.*, s.has_lead_art, s.lead_art_provider, s.lead_art_url, s.title, s.story_id
+		from facebook f1
+		inner join
+			(select link_url, max(run_time) as max_run_time from facebook group by link_url) f2 
+			on f1.link_url = f2.link_url and f1.run_time = f2.max_run_time
+		join 
+			seamus s
+			on f1.link_url = s.canonical_url
+			""")
+	dataset.freeze(result, format='csv', filename='output/insights_and_art.csv')
+
+
+
