8e39ab0fc09634940296fa2f86b8e1f0950a488c
David Eads <davideads@gmail.com>
2015-05-29 10:49:48 -0400

    add function to retroactively fix fb IDs (for posterity)

--- fabfile/data.py
+++ fabfile/data.py
@@ -5,9 +5,19 @@ Commands that update or process the application data.
 """
 import app_config
 import dataset
+import logging
+import requests
 
+from dateutil import parser
 from fabric.api import local, task
+from facebook import GraphAPI
 
+SECRETS = app_config.get_secrets()
+FACEBOOK_USER = 'NPR'
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 @task
 def local_bootstrap():
@@ -22,3 +32,36 @@ def drop_tables():
         print 'Dropping %s' % table
         table_obj = db[table]
         table_obj.drop()
+
+@task
+def fix_facebook_ids():
+    """
+    Retroactively adds Facebook IDs to Facebook table
+    """
+    db = dataset.connect(app_config.POSTGRES_URL)
+    table = db['facebook']
+
+    min_created_time_result = list(db.query('select min(created_time) as min_created_time from facebook'))
+    min_created_time = min_created_time_result[0]['min_created_time']
+
+    graph = GraphAPI(SECRETS['FACEBOOK_TOKEN'])
+    profile = graph.get_object(FACEBOOK_USER)
+    posts = graph.get_connections(profile['id'], 'posts')
+
+    done = False
+    while not done:
+        print 'processing batch of 25'
+        for post in posts['data']:
+            created_time = parser.parse(post['created_time'], ignoretz=True)
+            if created_time < min_created_time:
+                done = True
+                break
+
+            print 'updating %s (%s)' % (link, post['id'])
+            link = post['link'].split('?')[0]
+            table.update({
+                'facebook_id': post['id'],
+                'link_url': link,
+            }, ['link_url'])
+
+        posts = requests.get(posts['paging']['next']).json()
