dcf6211e5f001bd98ec8a16850f394d95bd8c2c6
David Eads <davideads@gmail.com>
2015-06-15 16:19:24 -0400

    function metric toggle and responsive design

--- www/js/app.js
+++ www/js/app.js
@@ -1,39 +1,25 @@
 // Global jQuery references
-var $commentCount = null;
+var $metricToggleButtons = null;
 
-/*
- * Run on page load.
- */
+var onToggleMetricView = function(e) {
+    console.log(this);
+    var $el = $(this)
+    var targetSelector = $el.parents('.btn-group').data('toggle-target');
+    var $target = $(targetSelector);
+    var toggleState = $el.find('input').attr('value');
+    if (toggleState == 'distribution') {
+        $target.find('.value').hide();
+        $target.find('.histogram').show();
+    } else {
+        $target.find('.value').show();
+        $target.find('.histogram').hide();
+    }
+}
+
 var onDocumentLoad = function(e) {
-}
-/*
- * Basic templating example.
- */
-var renderExampleTemplate = function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4),
-        'copy': JSON.stringify(COPY, null, 4)
-    });
+    $metricToggleButtons = $('.metric-toggle .btn');
 
-    var html = JST.example(context);
-
-    $('#template-example').html(html);
-}
-
-/*
- * Display the comment count.
- */
-var showCommentCount = function(count) {
-    $commentCount.text(count);
-
-    if (count > 0) {
-        $commentCount.addClass('has-comments');
-    }
-
-    if (count > 1) {
-        $commentCount.next('.comment-label').text('Comments');
-    }
+    $metricToggleButtons.on('click', onToggleMetricView);
 }
 
 $(onDocumentLoad);
