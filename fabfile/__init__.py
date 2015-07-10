5062b03fd5569804cfd055849099f59e18edbe91
David Eads <davideads@gmail.com>
2015-07-10 15:47:03 -0400

    let static server connect to the db too

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -94,7 +94,12 @@ def app(port='8000'):
     """
     Serve app.py.
     """
-    local('gunicorn -b 0.0.0.0:%s --timeout 3600 --debug --reload app:wsgi_app' % port)
+    if env.settings:
+        local("DEPLOYMENT_TARGET=%s bash -c 'gunicorn -b 0.0.0.0:%s --timeout 3600 --debug --reload app:wsgi_app'" % (env.settings, port))
+    else:
+        local('gunicorn -b 0.0.0.0:%s --timeout 3600 --debug --reload app:wsgi_app' % port)
+
+
 
 @task
 def public_app(port='8001'):
